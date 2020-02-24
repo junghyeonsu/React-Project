c0fedb8913d0f0459d60d64f51486f8aefaf3ad6
junghyeonsu <54893898+junghyeonsu@users.noreply.github.com>
2020-02-24 18:24:49 +0900

    다섯번째

--- Some Trending/server/module/test.js
+++ Some Trending/server/module/test.js
@@ -1,24 +1,36 @@
 const axios = require('axios');
-const API_KEY = "ff32a090cbc342f38a1ffdd2b6576e5e";
+const cheerio = require('cheerio');
 
-exports.getMovie = async () => {
+
+/* 비동기 방식으로 axios.get 으로 주소의 데이터를 받는다. */
+const getMelon = async () => {
     try {
-        return await axios.get(`http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=ff32a090cbc342f38a1ffdd2b6576e5e&targetDt=20200101`);
+      return await axios.get("https://music.bugs.co.kr/chart");
     } catch (error) {
-        console.log(error);
+        console.error(error);
     }
+};
+
+//frm > div > table
+// 만든 비동기적 요청함수를 불러서 데이터를 나눈다.
+exports.MelonCrawling =  (req,res) => {
+    getMelon().then(response => {
+        let ulList = [];
+        const $ = cheerio.load(response.data);
+        const $bodyList = $("#CHARTrealtime > table > tbody").children('tr');
+
+        $bodyList.each(function(i) {
+            ulList[i] = {
+                ranking : $(this).find('td div.ranking strong').text(),
+                thumbnail : $(this).find('td a.thumbnail img').attr('src'),
+                thumbnailAlt : $(this).find('td a.thumbnail img').attr('alt'),
+                title : $(this).find('th p a').text(),
+                artist : $(this).find('td p.artist a').text(),
+                album : $(this).find('td a.album').text()
+            }
+        })
+
+        res.send(ulList);
+    })
 }
 
-exports.getName = function(req,res){
-    res.send([
-        {
-            'name' : '정현수'
-        },
-        {
-            'name' : '하이'
-        },
-        {
-            'name' : '성공했니?'
-        }
-    ])
-}
--- Some Trending/server/server.js
+++ Some Trending/server/server.js
@@ -1,45 +1,21 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 var module = require('./module/test');
 
-const connection = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : '123456',
-    database : 'test'
-})
+// const connection = mysql.createConnection({
+//     host : 'localhost',
+//     user : 'root',
+//     password : '123456',
+//     database : 'test'
+// })
 
-app.get('/show',function(req,res){
-    var sql3 = 'select * from login';
-    connection.query(sql3,function(err,row,field){
-        if (err) throw err;
-
-        var user = [];  
-
-        for(var i = 0 ; i < row.length; i++) {
-            user.push({
-                uniqueId : row[i].id,
-                id : row[i].user_id,
-                password : row[i].password,
-                name : row[i].name,
-                register_date : row[i].register_date
-            })
-        }
-        res.send({
-            "user" : user,
-        })
-    })
-})
-
-app.get('/name', module.getName);
-
-app.get('/api/movie', module.getMovie);
+app.get('/api/melon',module.MelonCrawling);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
